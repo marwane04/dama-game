c17bc10a62973039b4e2431abdc45cb0796d0509
marwane <marwanefakir1949@gmail.com>
2026-04-16 19:27:46 +0100

    Add Piece model

---
+++ src/main/java/com/dama/model/Position.java
@@ -0,0 +1,35 @@
+package com.dama.model;
+
+public class Position {
+    int x;
+    int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+}
