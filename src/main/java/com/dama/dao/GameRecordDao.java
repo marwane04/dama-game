8dece33b33dc34d284bc29b9eff668012f48737e
marwane <marwanefakir1949@gmail.com>
2026-06-05 12:14:17 +0100

    fix: update GameRecordDao

--- src/main/java/com/dama/dao/GameRecordDao.java
+++ src/main/java/com/dama/dao/GameRecordDao.java
@@ -3,9 +3,10 @@ package com.dama.dao;
 import com.dama.controller.GameRecord;
 
 import java.sql.SQLException;
+import java.util.List;
 
 public interface GameRecordDao {
     void createTable() throws SQLException;
     void insert(GameRecord record) throws SQLException;
-    void findAll(GameRecord record) throws SQLException;
+    List<GameRecord> findAll() throws SQLException;
 }
